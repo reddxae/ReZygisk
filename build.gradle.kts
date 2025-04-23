5ce0637601628a0d3f2a17418c5cc7aaeca41560
Ivan <reddxae@proton.me>
2025-04-23 19:30:30 +0300

    feat: zygisksu -> rezygisk

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,9 @@ fun String.execute(currentWorkingDir: File = file("./")): String {
 val gitCommitCount = "git rev-list HEAD --count".execute().toInt()
 val gitCommitHash = "git rev-parse --verify --short HEAD".execute()
 
-val moduleId by extra("zygisksu")
+val moduleId by extra("rezygisk")
 val moduleName by extra("ReZygisk")
-val verName by extra("v1.0.0")
+val verName by extra("1.0.0")
 val verCode by extra(gitCommitCount)
 val commitHash by extra(gitCommitHash)
 val minAPatchVersion by extra(10655)
